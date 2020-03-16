6d023c07e6ed40d3d55d4bdbee6c1285a671b2ed
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-03-16 18:50:19 +0000

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -6,19 +6,14 @@
 
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
-def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
-  "SELECT projects.title, SUM(pledges.amount) FROM projects
+def selects_the_titles_of_all_projects_and_their_pledge_amounts
+"SELECT projects.title, SUM(pledges.amount) FROM projects
     INNER JOIN pledges
       ON projects.id = pledges.project_id
     GROUP BY projects.title;"
 end
 
-def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-  "SELECT user.name, user.age, SUM(user.pledges) FROM projects INNER JOIN pledges ON projects.id = pledges.project_id
-    GROUP BY projects.title;"
-end
-
-ef selects_the_user_name_age_and_pledge_amount_for_all_pledges
+def selects_the_user_name_age_and_pledge_amount_for_all_pledges
 "SELECT users.name, users.age, SUM(pledges.amount) FROM users
     INNER JOIN pledges
       ON users.id = pledges.user_id
